79f27784ee1839b4a015534a733842354408a6c2
Christopher Sanchez <shonen787@gmail.com>
2023-04-14 10:38:16 -0400

    Removed API functionality to get a working site down

--- js/gamesetup.js
+++ js/gamesetup.js
@@ -1,10 +1,11 @@
-const grid = {easy:4 ,medium:8, hard:10, reset:0};
-const gameImages = `<img class="card" src="img/background.png">`;
+const grid = {easy:4 ,medium:8, hard:9, reset:0};
+const gameImages = `<img class="cardFace cardFaceFront" src="img/background.png">`;
+
 
 // Get the elements into an array
 const menuButtons = document.getElementById("options");
 
-menuButtons.addEventListener("click", menuCallBack );
+menuButtons.addEventListener("click",menuCallBack);
 
 function menuCallBack(event){
     event.preventDefault();
@@ -12,59 +13,277 @@ function menuCallBack(event){
     initializeGameMap(grid[gameLevel]);
 }
 
+
 function initializeGameMap(gridSize){
+    const imageLocations = [
+        "0kaCC",
+        "2K9MO",
+        "58e7h",
+        "7QWm5",
+        "90uj0",
+        "AO9r5",
+        "D1Wls",
+        "FNAnV",
+        "H9s7M",
+        "IU4OU",
+        "JlnXU",
+        "Kasbu",
+        "NU6UT",
+        "PBpfa",
+        "QQfi3",
+        "RpMkt",
+        "SqdvK",
+        "Tq9b1",
+        "VrlQA",
+        "XGHJd",
+        "YSBGi",
+        "c3kJV",
+        "d16oA",
+        "fR3aY",
+        "j0yR1",
+        "jkPTA",
+        "lJ69w",
+        "mzNQJ",
+        "oipKU",
+        "qCZ0w",
+        "rLaXD",
+        "tlXMp",
+        "urMP8",
+        "wVPcR",
+        "z0i7Q",
+        "11pue",
+        "2yU1L",
+        "63R9b",
+        "7X7sE",
+        "95N42",
+        "BZasA",
+        "DHTsl",
+        "FV2KL",
+        "HBupq",
+        "J8vym",
+        "JwGeG",
+        "L4eQA",
+        "NuWw6",
+        "PW4ih",
+        "QuQLd",
+        "RvdwF",
+        "T3O26",
+        "UAIWT",
+        "W9AhT",
+        "Xbpqq",
+        "Z88sy",
+        "cLFrm",
+        "devDx",
+        "hCCFv",
+        "j292k",
+        "jlSgQ",
+        "lKaLf",
+        "nmZJ0",
+        "ol5Ud",
+        "qNNl1",
+        "rNIqi",
+        "ttgAP",
+        "uuhC2",
+        "xRQDc",
+        "zdYZN",
+        "21akY",
+        "34Uv4",
+        "63fUk",
+        "7ayLf",
+        "9KSW2",
+        "Bssrf",
+        "DUWSR",
+        "FWwQz",
+        "HEjl1",
+        "JJvCX",
+        "K0WEI",
+        "M6Yrc",
+        "OIH0E",
+        "PiyRK",
+        "R0GmI",
+        "SFILS",
+        "T3sSx",
+        "UHf3i",
+        "W9CO4",
+        "XpSrJ",
+        "ZLRTE",
+        "cMagS",
+        "eCAKO",
+        "hgqAo",
+        "jQvnL",
+        "jzWwo",
+        "lTqrz",
+        "nvBQY",
+        "p3hGc",
+        "qXYlx",
+        "sP1OR",
+        "u95wI",
+        "vR5HW",
+        "xip8a",
+        "21fNn",
+        "3ef2M",
+        "6FeNO",
+        "8RCjc",
+        "9OQVl",
+        "ByhbI",
+        "EMb9G",
+        "FkvJi",
+        "HHYaE",
+        "JNGrU",
+        "K98Fy",
+        "MC0To",
+        "OIR6d",
+        "Po6No",
+        "R6ewG",
+        "SHSpJ",
+        "TJNB2",
+        "Ubgml",
+        "WO2Id",
+        "XrUdT",
+        "ZcTYl",
+        "cP4Ui",
+        "eOV0G",
+        "i13OH",
+        "jRNpM",
+        "kq6B7",
+        "lgZ1H",
+        "ocD8y",
+        "pl1Cg",
+        "qweLQ",
+        "sY0GC",
+        "u9SUk",
+        "vwPmx",
+        "yAPXH",
+        "29ucV",
+        "3o8hc",
+        "6KsPM",
+        "8SRsf",
+        "9bo0J",
+        "BzQis",
+        "FF9qY",
+        "G1m7i",
+        "HNd9L",
+        "JiUNy",
+        "KETzN",
+        "N6lKD",
+        "OVtyf",
+        "QALCl",
+        "RIHhb",
+        "SYVNv",
+        "TUGkF",
+        "UtIWX",
+        "WOA5U",
+        "Y3gvB",
+        "amXcC",
+        "cQSJO",
+        "eSA1k",
+        "imI21",
+        "jRsKm",
+        "lAig1",
+        "mPoex",
+        "odGvv",
+        "poxQN",
+        "r0d6v",
+        "sjepC",
+        "uFP3j",
+        "w3nOs",
+        "yBHME",
+        "2JR6X",
+        "4Q1Bs",
+        "6vvaq",
+        "8lYd6",
+        "AGDru",
+        "CM3hQ",
+        "FJRV6",
+        "GqKRb",
+        "Hpqrq",
+        "JjHlE",
+        "KTs2K",
+        "N8sRb",
+        "Ohu4j",
+        "QGwgW",
+        "RNYPr",
+        "Sk1sg",
+        "Todjq",
+        "V12fj",
+        "We5kT",
+        "YAzwK",
+        "ba9S0",
+        "cXkLD",
+        "fPj3U",
+        "iwldr",
+        "jTOnG",
+        "lGm9f",
+        "mpgCa",
+        "ohW8o",
+        "q28lK",
+        "r6Rer",
+        "sn974",
+        "uYnw7",
+        "wTv1d",
+        "yx633"
+    ];
     const gameArea = document.getElementById('gameContainer');
     //Clear Board
     gameArea.innerHTML = '';
-    
     if(gridSize === 0) return;
 
     const cardGridItemSize = Number(getComputedStyle(gameArea).width.replace("px",''))/gridSize;
+    const grideSize2 = gridSize**2;
     let columnTemplate = '';
 
     for (let i=1;i <= gridSize;i++){
         columnTemplate += `${cardGridItemSize}px `;
     }
-    console.log(columnTemplate);
+
     gameArea.style.gridTemplateColumns = columnTemplate;
 
+    let chosenImages = [];
+    for(let i=0;i<(grideSize2/2);i++){
+        const randomNumber = Math.floor(Math.random() * (imageLocations.length - 1))
+        if (imageLocations[randomNumber] !== undefined){
+            chosenImages[i] = imageLocations[randomNumber];
+            delete imageLocations[randomNumber];
+        }else{
+            i--;
+        }
+               
+    }
+    {
+        const tempArray = [...chosenImages];
+        for (item in tempArray){
+            chosenImages.push(tempArray[item]);
+        }
+    }   
+
+    shuffle(chosenImages);
+
     //Add Images
-    for (let i=0;i < gridSize**2;i++){
+    for (let i=0;i < grideSize2;i++){
+        //Add div and card backs
         const card = document.createElement('div');
-        card.innerHTML=gameImages;
+        card.className = "card";
+        card.innerHTML = gameImages;
+        card.innerHTML += `<img class=cardFace cardFaceBack src="./img/${chosenImages[i]}">`;
         gameArea.appendChild(card);
     }
-
-    
-
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //Listeners on each button
-// for (let i=0; i < button.length; i++){
-//     button[i].addEventListener("click", function(){
-//         changeGridSize(Number(i));
-//         if(grid === 0){
-//             document.getElementById('game-section').innerHTML ='';
-
-//         }else{
-//         document.getElementById('game-section').innerHTML +=  gameImages;
-//         }
-//     });
-// }
-
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
